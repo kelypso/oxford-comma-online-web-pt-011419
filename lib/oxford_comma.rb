ac5e8bdafbb0bfb3c014754600ef628271aa9812
Kelsey Low <kklowlow@gmail.com>
2019-01-22 02:13:58 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,8 +3,15 @@ def oxford_comma(array)
     array.join
   elsif array.length == 2 
     array.join(" and ")
+  elsif array.length >= 3
+  array[0..2].join(", ") + ", and" array[-1]
+  end
+end
+  
+=begin
   elsif array.length > 2
     array.last.prepend("and ")
     array.join(", ")
   end
 end
+=end
